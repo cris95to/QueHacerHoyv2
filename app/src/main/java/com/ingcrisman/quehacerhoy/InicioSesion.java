ce9dee4bce3de22d092fae7230e079b6f1313bc4
cristian torres <ctorresolarte@gmail.com>
2021-11-07 09:23:44 -0500

    commit creacion de interfaces de inicio de sesion y registro

---
+++ app/src/main/java/com/ingcrisman/quehacerhoy/InicioSesion.java
@@ -0,0 +1,23 @@
+package com.ingcrisman.quehacerhoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+
+    public void goToMenu(View view) {
+        Intent newIntent = new Intent(this, MainActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(newIntent);
+    }
+
+}
---
+++ app/src/main/java/com/ingcrisman/quehacerhoy/Registro.java
@@ -0,0 +1,22 @@
+package com.ingcrisman.quehacerhoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+    public void goToMenu(View view) {
+        Intent newIntent = new Intent(this, MainActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(newIntent);
+    }
+}
