a42eab1f11a8646a28b471a17b7e53432000d72a
cristian torres <ctorresolarte@gmail.com>
2021-11-08 06:21:45 -0500

    conexion con firebase y  creacion de un registro de ususario aun en progreso

--- app/src/main/java/com/ingcrisman/quehacerhoy/Registro.java
+++ app/src/main/java/com/ingcrisman/quehacerhoy/Registro.java
@@ -5,13 +5,70 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText correo;
+    private EditText contraseña;
+    private EditText contraseñaConfirmacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.correo);
+        contraseña = findViewById(R.id.Contraseña2);
+        contraseñaConfirmacion = findViewById(R.id.confirmacionContraseña);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrarUsuario(View view){
+
+        if (contraseña.getText().equals(contraseñaConfirmacion.getText())) {
+
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+
+        
+    }
+        private void firebaseAuthWithGoogle(String idToken) {
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithCredential:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
     }
 
     public void goToMenu(View view) {
